d71ddaba89288d9e194a282aa423f875e4a8b522
Edvard Kraljevic <edo.kraljevic1@gmail.com>
2017-05-28 23:18:03 +0200

    Merge branch 'master' of C:\Users\Edo\Documents\GitHub\E-Dnevnik with conflicts.

---
+++ app/src/main/java/com/example/sviostali/e_dnevnik/ItemList.java
@@ -0,0 +1,12 @@
+package com.example.sviostali.e_dnevnik;
+
+public class ItemList {
+
+    public String username;
+    public int logo;
+
+    ItemList(String username, int logo){
+        this.username = username;
+        this.logo = logo;
+    }
+}
---
+++ app/src/main/java/com/example/sviostali/e_dnevnik/MyListAdapter.java
@@ -0,0 +1,37 @@
+package com.example.sviostali.e_dnevnik;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Edo on 5/27/2017.
+ */
+
+public class MyListAdapter extends BaseAdapter {
+
+
+
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
